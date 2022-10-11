d8c63015e9307e2a8a9e4af685a75d01a5b13175
Leonardo Gudini <leonardo.gudini98@gmail.com>
2022-10-11 15:02:02 +0200

    add: category migration, model function posts and category

--- app/Post.php
+++ app/Post.php
@@ -2,9 +2,14 @@
 
 namespace App;
 
+use App\Category;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    protected $fillable=['title', 'slug', 'description'];
+    protected $fillable=['title', 'slug', 'description', 'category'];
+    
+    public function category(){
+        return $this->belongsTo('App\Category');
+    }
 }
